c454e44070af379779fb363dd75cc0862d2f3352
Tall Sequoia <24196105+tallSequoia@users.noreply.github.com>
2022-12-03 22:59:26 +0000

    Update __init__.py

--- components/ftms/__init__.py
+++ components/ftms/__init__.py
@@ -2,7 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
-ftms_bike_ns = cg.esphome_ns.namespace('ftms')
+ftms_bike_ns = cg.esphome_ns.namespace('ftms_bike')
 FTMSBike = ftms_bike_ns.class_('FTMSBike', cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
