84e7c6d61ad4a449b29b3cd265de458db9f0fa93
Tall Sequoia <24196105+tallSequoia@users.noreply.github.com>
2022-12-08 23:24:48 +0000

    Update ftms_bike_server.cpp

--- components/ftms_bike_server/ftms_bike_server.cpp
+++ components/ftms_bike_server/ftms_bike_server.cpp
@@ -7,13 +7,6 @@
 
 #ifdef USE_ESP32
 
-#include <nvs_flash.h>
-#include <freertos/FreeRTOSConfig.h>
-#include <esp_bt_main.h>
-#include <esp_bt.h>
-#include <freertos/task.h>
-#include <esp_gap_ble_api.h>
-
 namespace ftms {
 namespace bike_server {
 
@@ -50,7 +43,7 @@ void FTMSBikeServer::setup() {
   advertising->setScanResponse(true);
   advertising->setMinPreferred(0x06);
 
-  BLEDevice::startAdvertising();
+  advertising->start();
 }
 
 void FTMSBikeServer::loop() {
