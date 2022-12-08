d7d041d177119fcd32708f8d56ea51fd9ffb472a
Tall Sequoia <24196105+tallSequoia@users.noreply.github.com>
2022-12-08 22:23:38 +0000

    Update __init__.py

--- components/ftms/__init__.py
+++ components/ftms/__init__.py
@@ -1,14 +1,20 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, ESP_PLATFORM_ESP32
+from esphome.core import coroutine_with_priority
 
-ftms_ns = cg.esphome_ns.namespace('ftms')
-FTMS = ftms_ns.class_('FTMS', cg.Component)
+ESP_PLATFORMS = [ESP_PLATFORM_ESP32]
+CODEOWNERS = ['@jesserockz']
+
+ftms_bike_server_ns = cg.esphome_ns.namespace('ftms_bike_server')
+FTMSBikeServer = ftms_bike_server_ns.class_('FTMSBikeServer', cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(FTMS)
+    cv.GenerateID(): cv.declare_id(FTMSBikeServer),
 }).extend(cv.COMPONENT_SCHEMA)
+
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
+    cg.add_define('USE_BLE_SERVER')
