eaa2ca0215a237a608d3740363f1a81f4e615443
Tall Sequoia <24196105+tallSequoia@users.noreply.github.com>
2022-12-10 21:52:14 +0000

    Update esp32_ftms_bike.h

--- components/esp32_ftms_bike/esp32_ftms_bike.h
+++ components/esp32_ftms_bike/esp32_ftms_bike.h
@@ -19,28 +19,21 @@ using namespace esp32_ble_server;
 class ESP32FtmsBikeComponent : public Component, public BLEServiceComponent {
  public:
   ESP32FtmsBikeComponent();
-  void dump_config() override;
-  void loop() override;
   void setup() override;
-  void setup_characteristics();
+  void loop() override;
+  void start() override;
+  void stop() override;
   void on_client_connect() override;
   void on_client_disconnect() override;
 
+  void dump_config() override;
   float get_setup_priority() const override;
 
   void set_authorizer(binary_sensor::BinarySensor *authorizer) { this->authorizer_ = authorizer; }
   void set_status_indicator(output::BinaryOutput *status_indicator) { this->status_indicator_ = status_indicator; }
 
  protected:
-  bool setup_complete_{false};
-
-  uint32_t identify_start_{0};
-  uint32_t identify_duration_;
-  uint32_t authorized_start_{0};
-  uint32_t authorized_duration_;
-
   std::vector<uint8_t> incoming_data_;
-  wifi::WiFiAP connecting_sta_;
 
   std::shared_ptr<BLEService> service_;
   BLECharacteristic *featuresCharacteristic_;
