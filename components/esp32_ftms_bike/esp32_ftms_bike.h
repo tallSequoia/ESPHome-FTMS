c31c1a8ed1e708b003317bac2109923c3c1ce06b
Tall Sequoia <24196105+tallSequoia@users.noreply.github.com>
2022-12-12 22:43:56 +0000

    Update esp32_ftms_bike.h

--- components/esp32_ftms_bike/esp32_ftms_bike.h
+++ components/esp32_ftms_bike/esp32_ftms_bike.h
@@ -4,12 +4,17 @@
 #include "esphome/core/helpers.h"
 #include "esphome/core/preferences.h"
 
+#include "esphome/components/esp32_ble_server/ble_server.h"
+#include "esphome/components/esp32_ble/ble_advertising.h"
+#include "esphome/components/esp32_ble/ble_uuid.h"
+#include "esphome/components/esp32_ble/queue.h"
+
 #ifdef USE_ESP32
 
 namespace esphome {
 namespace esp32_ftms_bike {
 
-using namespace esp32_ble_server;
+//using namespace esp32_ble_server;
 
 class FTMSBike : public esp32_ble_server::BLEServer {
 
