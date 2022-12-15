28eb01b48a4598a2f51b834e4e6c5a30a6a29c57
Tall Sequoia <24196105+tallSequoia@users.noreply.github.com>
2022-12-15 21:10:33 +0000

    Update esp32_ftms_bike.h

--- components/esp32_ftms_bike/esp32_ftms_bike.h
+++ components/esp32_ftms_bike/esp32_ftms_bike.h
@@ -14,14 +14,30 @@
 namespace esphome {
 namespace esp32_ftms_bike {
 
-//using namespace esp32_ble_server;
-
 class FTMSBike : public esp32_ble_server::BLEServer {
   public:
     void setup();
     void loop();
-};
+    void dump_config();
+    void teardown();
 
+    void set_manufacturer(const std::string &manufacturer);
+    void set_model(const std::string &model);
+
+    bool can_proceed();
+
+    std::shared_ptr<BLEService> create_service(const uint8_t *uuid, bool advertise = false);
+    std::shared_ptr<BLEService> create_service(uint16_t uuid, bool advertise = false);
+    std::shared_ptr<BLEService> create_service(const std::string &uuid, bool advertise = false);
+    std::shared_ptr<BLEService> create_service(ESPBTUUID uuid, bool advertise = false, uint16_t num_handles = 15, uint8_t inst_id = 0);
+
+    uint32_t get_connected_client_count();
+
+    esp_gatt_if_t get_gatts_if();
+    void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param);
+
+    void register_service_component(BLEServiceComponent *component);
+};
 
 }  // namespace esp32_ftms_bike
 }  // namespace esphome
