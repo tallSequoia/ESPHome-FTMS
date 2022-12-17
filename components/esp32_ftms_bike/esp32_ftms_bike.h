76777adad64a6de4c144dfe9c5d753c6ea895cd6
Tall Sequoia <24196105+tallSequoia@users.noreply.github.com>
2022-12-17 23:04:48 +0000

    Update esp32_ftms_bike.h

--- components/esp32_ftms_bike/esp32_ftms_bike.h
+++ components/esp32_ftms_bike/esp32_ftms_bike.h
@@ -5,7 +5,6 @@
 #include "esphome/core/component.h"
 #include "esphome/core/helpers.h"
 #include "esphome/core/preferences.h"
-#include "esphome/core/defines.h"
 
 #include "esphome/components/esp32_ble_server/ble_server.h"
 #include "esphome/components/esp32_ble_server/ble_characteristic.h"
@@ -33,12 +32,12 @@ namespace esp32_ftms_bike {
       void set_model(const std::string &model) { this->model_ = model; }
 
       bool can_proceed();
-
+/*
       std::shared_ptr<FTMSBike> create_service(const uint8_t *uuid, bool advertise = false);
       std::shared_ptr<FTMSBike> create_service(uint16_t uuid, bool advertise = false);
       std::shared_ptr<FTMSBike> create_service(const std::string &uuid, bool advertise = false);
       std::shared_ptr<FTMSBike> create_service(ESPBTUUID uuid, bool advertise = false, uint16_t num_handles = 15, uint8_t inst_id = 0);
-
+*/
       // JM: I needed to provide the method definition here as it did not inherit from the base, possibly due to being in the header, not the "main" class? Yuck!
       esp_gatt_if_t get_gatts_if() { return this->gatts_if_; }
       uint32_t get_connected_client_count() { return this->connected_clients_; }
