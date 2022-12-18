3a2ec951142ea059e2de27ae8a383bebb196cc37
Tall Sequoia <24196105+tallSequoia@users.noreply.github.com>
2022-12-18 23:28:18 +0000

    Update esp32_ftms_bike.h

--- components/esp32_ftms_bike/esp32_ftms_bike.h
+++ components/esp32_ftms_bike/esp32_ftms_bike.h
@@ -3,14 +3,12 @@
 #include "esphome/core/component.h"
 
 namespace esphome {
-namespace esp32_ftms_bike {
-
-class FTMSBike : public Component {
- public:
-  void setup() override;
-  void loop() override;
-  void dump_config() override;
-};
-
-}  // namespace esp32_ftms_bike
+ namespace esp32_ftms_bike {
+  class FTMSBike : public Component {
+   public:
+    void setup() override;
+    void loop() override;
+    void dump_config() override;
+  }
+ }  // namespace esp32_ftms_bike
 }  // namespace esphome
