060529bd58ca5a5c0fb0931e87e2b35a49b85348
Tall Sequoia <24196105+tallSequoia@users.noreply.github.com>
2022-12-21 21:30:20 +0000

    Update esp32_ftms_bike.h

--- components/esp32_ftms_bike/esp32_ftms_bike.h
+++ components/esp32_ftms_bike/esp32_ftms_bike.h
@@ -17,43 +17,39 @@
 
 #ifdef USE_ESP32
 
+using namespace esphome;
 using namespace esp32_ble_server;
 
-namespace esphome {
 namespace esp32_ftms_bike {
-  class FTMSBike : public esphome::esp32_ble_server::BLEServer {
-    public:
-      void setup();
-      void loop();
-      void dump_config();
-      void teardown();
-      void update() {};   // This is needed for esp-idf
 
-      void set_parent(esphome::esp32_ble_server::BLEServer *parent) { parent_ = parent; }
+class FTMSBike : public BLEServer {
+  public:
+    void setup();
+    void loop();
+    void dump_config();
+    void teardown();
+    void update(){};    // esp-idf needs this, whereas arduino does not
 
-      // JM: I needed to provide the method definition here as it did not inherit from the base, possibly due to being in the header, not the "main" class? Yuck!
-      void set_manufacturer(const std::string &manufacturer) { this->manufacturer_ = manufacturer; }
-      void set_model(const std::string &model) { this->model_ = model; }
+    // JM: I needed to provide the method definition here as it did not inherit from the base, possibly due to being in the header, not the "main" class? Yuck!
+    void set_manufacturer(const std::string &manufacturer) { this->manufacturer_ = manufacturer; }
+    void set_model(const std::string &model) { this->model_ = model; }
 
-      bool can_proceed();
+    bool can_proceed();
 
-      std::shared_ptr<FTMSBike> create_service(const uint8_t *uuid, bool advertise = false);
-      std::shared_ptr<FTMSBike> create_service(uint16_t uuid, bool advertise = false);
-      std::shared_ptr<FTMSBike> create_service(const std::string &uuid, bool advertise = false);
-      std::shared_ptr<FTMSBike> create_service(ESPBTUUID uuid, bool advertise = false, uint16_t num_handles = 15, uint8_t inst_id = 0);
+    std::shared_ptr<FTMSBike> create_service(const uint8_t *uuid, bool advertise = false);
+    std::shared_ptr<FTMSBike> create_service(uint16_t uuid, bool advertise = false);
+    std::shared_ptr<FTMSBike> create_service(const std::string &uuid, bool advertise = false);
+    std::shared_ptr<FTMSBike> create_service(ESPBTUUID uuid, bool advertise = false, uint16_t num_handles = 15, uint8_t inst_id = 0);
 
-      // JM: I needed to provide the method definition here as it did not inherit from the base, possibly due to being in the header, not the "main" class? Yuck!
-      esp_gatt_if_t get_gatts_if() { return this->gatts_if_; }
-      uint32_t get_connected_client_count() { return this->connected_clients_; }
-      const std::map<uint16_t, void *> &get_clients() { return this->clients_; }
+    // JM: I needed to provide the method definition here as it did not inherit from the base, possibly due to being in the header, not the "main" class? Yuck!
+    esp_gatt_if_t get_gatts_if() { return this->gatts_if_; }
+    uint32_t get_connected_client_count() { return this->connected_clients_; }
+    const std::map<uint16_t, void *> &get_clients() { return this->clients_; }
 
-      void register_service_component(BLEServiceComponent *component);
-
-    protected:
-      esphome::esp32_ble_server::BLEServer *parent_;
-  };
+    void register_service_component(BLEServiceComponent *component);
+};
 
 }  // namespace esp32_ftms_bike
-}  // namespace esphome
+//}  // namespace esphome
 
 #endif
