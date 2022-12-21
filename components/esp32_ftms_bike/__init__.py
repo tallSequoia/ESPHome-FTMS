130aae7ec8f0cb5da114733d07f87169cfe6f58e
Tall Sequoia <24196105+tallSequoia@users.noreply.github.com>
2022-12-21 21:28:35 +0000

    Update __init__.py

--- components/esp32_ftms_bike/__init__.py
+++ components/esp32_ftms_bike/__init__.py
@@ -2,30 +2,27 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_MODEL
 from esphome.components import esp32_ble
-from esphome.components import esp32_ble_server
 from esphome.core import CORE
 from esphome.components.esp32 import add_idf_sdkconfig_option
 
-AUTO_LOAD = ["esp32_ble_server","esp32_ble"] 
+AUTO_LOAD = ["esp32_ble"]
+CODEOWNERS = ["@tallsequoia"]
 CONFLICTS_WITH = ["esp32_ble_tracker", "esp32_ble_beacon"]
 DEPENDENCIES = ["esp32"]
-CODEOWNERS = ["@tallsequoia"]
 
 CONF_MANUFACTURER = "manufacturer"
 CONF_BLE_ID = "ble_id"
 
-esp32_ftms_bike_ns = cg.global_ns.namespace("esp32_ftms_bike")
-FTMSBike = esp32_ftms_bike_ns.class_("FTMSBike", cg.Component)
-
 esp32_ble_server_ns = cg.esphome_ns.namespace("esp32_ble_server")
+BLEServer = esp32_ble_server_ns.class_("BLEServer", cg.Component)
 BLEServiceComponent = esp32_ble_server_ns.class_("BLEServiceComponent")
 
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.declare_id(FTMSBike),
-        cv.GenerateID(CONF_BLE_ID): cv.use_id(esp32_ble_server.BLEServer), # cv.use_id(esp32_ble.ESP32BLE),
-        cv.Optional(CONF_MANUFACTURER, default="TallSequoia"): cv.string,
+        cv.GenerateID(): cv.declare_id(BLEServer),
+        cv.GenerateID(CONF_BLE_ID): cv.use_id(esp32_ble.ESP32BLE),
+        cv.Optional(CONF_MANUFACTURER, default="ESPHome"): cv.string,
         cv.Optional(CONF_MODEL): cv.string,
     }
 ).extend(cv.COMPONENT_SCHEMA)
@@ -41,8 +38,7 @@ async def to_code(config):
         cg.add(var.set_model(config[CONF_MODEL]))
     cg.add_define("USE_ESP32_BLE_SERVER")
 
-#    cg.add(parent.set_server(var))
-    cg.add(var.set_parent(parent))  # JM add (based on MQTT Subscribe Sensor example setting parent of MQTT Client Component)
+    cg.add(parent.set_server(var))
 
     if CORE.using_esp_idf:
         add_idf_sdkconfig_option("CONFIG_BT_ENABLED", True)
