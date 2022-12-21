d75815853653fbf705c7a09a2dce3aac13046063
Tall Sequoia <24196105+tallSequoia@users.noreply.github.com>
2022-12-21 22:38:49 +0000

    Update esp32_ftms_bike.cpp

--- components/esp32_ftms_bike/esp32_ftms_bike.cpp
+++ components/esp32_ftms_bike/esp32_ftms_bike.cpp
@@ -1,11 +1,11 @@
 #include "esp32_ftms_bike.h"
 
+#ifdef USE_ESP32
+
 #include "esphome/components/esp32_ble/ble.h"
 #include "esphome/core/log.h"
 #include "esphome/core/application.h"
 #include "esphome/core/version.h"
-
-#ifdef USE_ESP32
 
 #include <nvs_flash.h>
 #include <freertos/FreeRTOSConfig.h>
@@ -13,6 +13,19 @@
 #include <esp_bt.h>
 #include <freertos/task.h>
 #include <esp_gap_ble_api.h>
+
+// From the dot h
+#include "esphome/components/esp32_ble_server/ble_server.h"
+#include "esphome/components/esp32_ble_server/ble_characteristic.h"
+//#include "esphome/components/esp32_ble/ble_advertising.h"
+//#include "esphome/components/esp32_ble/ble_uuid.h"
+//#include "esphome/components/esp32_ble/queue.h"
+//#include <esp_gap_ble_api.h>
+//#include <esp_gatts_api.h>
+
+#include "esphome/components/binary_sensor/binary_sensor.h"
+#include "esphome/components/output/binary_output.h"
+
 
 namespace esphome {
 namespace esp32_ftms_bike {
