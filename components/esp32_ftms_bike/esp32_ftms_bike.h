bb87c5d718d8f663b149e6157ca30f9181a3ab40
Tall Sequoia <24196105+tallSequoia@users.noreply.github.com>
2022-12-21 22:36:06 +0000

    Update esp32_ftms_bike.h

--- components/esp32_ftms_bike/esp32_ftms_bike.h
+++ components/esp32_ftms_bike/esp32_ftms_bike.h
@@ -20,9 +20,9 @@
 
 #ifdef USE_ESP32
 
-using namespace esphome;
 using namespace esp32_ble_server;
 
+namespace esphome {
 namespace esp32_ftms_bike {
 
 class FTMSBike : public BLEServiceComponent {
@@ -39,8 +39,8 @@ class FTMSBike : public BLEServiceComponent {
     void on_client_disconnect();
 
     // JM: I needed to provide the method definition here as it did not inherit from the base, possibly due to being in the header, not the "main" class? Yuck!
-//    void set_manufacturer(const std::string &manufacturer) { this->manufacturer_ = manufacturer; }
-//    void set_model(const std::string &model) { this->model_ = model; }
+    void set_manufacturer(const std::string &manufacturer) { this->manufacturer_ = manufacturer; }
+    void set_model(const std::string &model) { this->model_ = model; }
 
     std::shared_ptr<FTMSBike> create_service(const uint8_t *uuid, bool advertise = false);
     std::shared_ptr<FTMSBike> create_service(uint16_t uuid, bool advertise = false);
@@ -48,14 +48,44 @@ class FTMSBike : public BLEServiceComponent {
     std::shared_ptr<FTMSBike> create_service(ESPBTUUID uuid, bool advertise = false, uint16_t num_handles = 15, uint8_t inst_id = 0);
 
     // JM: I needed to provide the method definition here as it did not inherit from the base, possibly due to being in the header, not the "main" class? Yuck!
-//    esp_gatt_if_t get_gatts_if() { return this->gatts_if_; }
+    esp_gatt_if_t get_gatts_if() { return this->gatts_if_; }
     uint32_t get_connected_client_count() { return this->connected_clients_; }
     const std::map<uint16_t, void *> &get_clients() { return this->clients_; }
 
     void register_service_component(BLEServiceComponent *component);
+
+  protected:
+    bool create_device_characteristics_();
+
+    void add_client_(uint16_t conn_id, void *client) {
+      this->clients_.insert(std::pair<uint16_t, void *>(conn_id, client));
+    }
+    bool remove_client_(uint16_t conn_id) { return this->clients_.erase(conn_id) > 0; }
+
+    bool can_proceed_{false};
+
+    std::string manufacturer_;
+    optional<std::string> model_;
+    esp_gatt_if_t gatts_if_{0};
+    bool registered_{false};
+
+    uint32_t connected_clients_{0};
+    std::map<uint16_t, void *> clients_;
+
+    std::vector<std::shared_ptr<BLEService>> services_;
+    std::shared_ptr<BLEService> device_information_service_;
+
+    std::vector<BLEServiceComponent *> service_components_;
+
+    enum State : uint8_t {
+      INIT = 0x00,
+      REGISTERING,
+      STARTING_SERVICE,
+      RUNNING,
+    } state_{INIT};
 };
 
 }  // namespace esp32_ftms_bike
-//}  // namespace esphome
+}  // namespace esphome
 
 #endif
